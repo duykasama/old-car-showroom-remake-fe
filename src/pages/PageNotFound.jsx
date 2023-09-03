320b6c10a7c5e4d4c4386b18e6fab31ed32f2549
duykasama <duykasama@gmail.com>
2023-09-03 16:30:14 +0700

    add 404 page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+
 function PageNotFound() {
-    return ( <main>The requested content doesn't exist</main> );
+  return (
+    <main className="flex flex-col justify-center items-center gap-4 h-96 my-32">
+      <h2 className="font-semibold">OOPS! PAGE NOT FOUND</h2>
+      <div className="text-9xl font-extrabold">404</div>
+      <h1 className="text-2xl font-bold">
+        WE ARE SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND
+      </h1>
+      <button onClick={() => history.back()} className="py-2 px-4 bg-slate-800 rounded text-white font-semibold flex items-center gap-2 hover:scale-110 hover:shadow-slate-700 shadow-md transition">
+        <FontAwesomeIcon icon={faArrowLeftLong} />
+        <span>Go back</span>
+      </button>
+    </main>
+  );
 }
 
 export default PageNotFound;
