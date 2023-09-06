c4e2cf21c27891869010b118f65217a117871b78
duykasama <duykasama@gmail.com>
2023-09-07 06:46:22 +0700

    add page loader

--- src/pages/user/CarInventory.jsx
+++ src/pages/user/CarInventory.jsx
@@ -3,6 +3,8 @@ import SearchBar from "../../components/SearchBar";
 import fetchPosts, { fetchLastPage } from "../../lib/api/fetchPosts";
 import Post from "../../components/post/Post";
 import Pagination from "../../components/Pagination";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 const PAGE_SIZE = 12;
 
@@ -11,15 +13,20 @@ function CarInventory() {
   const [currentPage, setCurrentPage] = useState(1);
   const [currentPageTracker, setCurrentPageTracker] = useState(1);
   const [lastPage, setLastPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     getPosts(currentPage);
+
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   }, [currentPage]);
 
   async function getPosts(offset) {
     setPosts(await fetchPosts(PAGE_SIZE, offset));
     setLastPage(await fetchLastPage(PAGE_SIZE));
+    await new Promise((r) => setTimeout(r, 500));
+    setIsLoading(false);
   }
 
   const handlePaginationSubmit = (event) => {
@@ -32,9 +39,22 @@ function CarInventory() {
   return (
     <main className="grid grid-cols-5 px-20 my-10 gap-6">
       <SearchBar />
-      <div className="col-span-4 grid grid-cols-4 grid-rows-3 gap-8">
-        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
-      </div>
+      {isLoading && (
+        <div className="col-span-4 w-full h-full flex justify-center items-center">
+          <FontAwesomeIcon icon={faSpinner} className="text-5xl animate-spin" />
+        </div>
+      )}
+      {posts && posts.length > 0 && !isLoading ? (
+        <div className="col-span-4 grid grid-cols-4 grid-rows-3 gap-8">
+          {posts.map((post) => (
+            <Post key={post.id} post={post} />
+          ))}
+        </div>
+      ) : (
+        <div className="col-span-4 grid grid-cols-4 grid-rows-3 gap-8">
+          There is no posts
+        </div>
+      )}
       <div className="col-span-5">
         <Pagination
           handlePaginationSubmit={handlePaginationSubmit}
--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -4,24 +4,35 @@ import PrioritizedPosts from "../../components/ui/user/PrioritizedPosts";
 import WebInfo from "../../components/ui/user/webinfo/WebInfo";
 import fetchPosts from "../../lib/api/fetchPosts";
 import useAuth from "../../hooks/useAuth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   const [priorPosts, setPriorPosts] = useState([]);
-  const {auth} = useAuth();
+  const [isLoading, setIsloading] = useState(false);
+  const { auth } = useAuth();
 
   useEffect(() => {
+    setIsloading(true);
     fetchPriorPosts();
     console.log(auth);
   }, []);
 
   const fetchPriorPosts = async () => {
     setPriorPosts(await fetchPosts(9, 1));
+    await new Promise((r) => setTimeout(r, 500));
+    setIsloading(false);
   };
 
   return (
     <main className="flex flex-col justify-between items-center gap-28">
       <Hero />
-      <PrioritizedPosts posts={priorPosts} />
+      {isLoading ? (
+        <FontAwesomeIcon icon={faSpinner} className="text-5xl animate-spin" />
+      ) : (
+        <PrioritizedPosts posts={priorPosts} />
+      )}
+
       <WebInfo />
     </main>
   );
